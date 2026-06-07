6a32d7503562370e3af4547972a38d30258cc788
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-06-07 22:19:25 +0500

    Delete plugins/menu.js

--- plugins/menu.js
+++
@@ -1,8 +0,0 @@
-mndMessage(msg.key.remoteJid, {
-            text: "RED ZONE MD MENU\n. ping\n. menu"
-        })
-    }
-}odule.exports = {
-    pattern: "menu",
-    run: async (sock, msg, args) => {
-        await sock.se
