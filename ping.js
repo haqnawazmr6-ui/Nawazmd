e818d91bab8fe38d39fe1e1d7fb5e45d0aa7b276
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-06-07 22:18:35 +0500

    Delete ping.js

--- ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pattern: "ping",
-    run: async (sock, msg, args) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: "PONG ⚡ RED ZONE MD is alive"
-        })
-    }
-}
