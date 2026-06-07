22f60851cdfbbbeed9d7b508021cc397cd195e2c
haqnawazmr6-ui <haqnawazmr6@gmail.com>
2026-06-07 22:12:02 +0500

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pattern: "menu",
-    run: async (sock, msg, args) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: "RED ZONE MD MENU\n. ping\n. menu"
-        })
-    }
-}
